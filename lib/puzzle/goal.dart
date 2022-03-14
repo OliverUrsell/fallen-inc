0e1796171f235ffee5a581eb8b1d92ad462e7f51
OliverUrsell <ollie@fuzzylabs.ai>
2022-03-14 19:53:32 +0000

    Goal can't go up

--- lib/puzzle/goal.dart
+++ lib/puzzle/goal.dart
@@ -5,7 +5,7 @@ class Goal extends Block{
 
   Goal() : super(
     imagePath: "assets/MiniArella/arella_mini_forwards_1.png",
-    walls: const Walls(),
+    walls: const Walls(up: false),
     selected: false,
     onTap: null,
     movable: false,
